919d46675ee9f86155fbdcf96100662cbfb3ad14
Johnathan Tam <johnathantam@Johnathans-MBP.gateway.sasktel.net>
2020-09-18 22:05:18 -0600

    Refactored authentication to use the native react auth0 hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Auth0Provider } from './auth/react-auth0-spa';
+import { Auth0Provider } from "@auth0/auth0-react";
 import { AUTH_CONFIG } from './auth/auth-config';
 import * as serviceWorker from './serviceWorker';
+import App from './App';
 
-const onRedirectCallback = appState => {
-  window.history.replaceState(
-    {},
-    document.title,
-    appState && appState.targetUrl
-      ? appState.targetUrl
-      : window.location.pathname
-  );
-};
+// const onRedirectCallback = appState => {
+//   window.history.replaceState(
+//     {},
+//     document.title,
+//     appState && appState.targetUrl
+//       ? appState.targetUrl
+//       : window.location.pathname
+//   );
+// };
 
 ReactDOM.render(
   <Auth0Provider
     domain={AUTH_CONFIG.domain}
-    client_id={AUTH_CONFIG.clientId}
-    redirect_uri={AUTH_CONFIG.callbackUrl}
-    onRedirectCallback={onRedirectCallback}
+    clientId={AUTH_CONFIG.clientId}
+    redirectUri={AUTH_CONFIG.callbackUrl}
+    // onRedirectCallback={onRedirectCallback}
     audience={AUTH_CONFIG.audience}
     useRefreshTokens={true}
-  />,
+  >
+    <App />
+  </Auth0Provider>,
   document.getElementById('root')
 );
 
